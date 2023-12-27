1f2e53d59092c27dc857f7366326ba6cae3095bb
Ali Cagatay <aliccagatay@gmail.com>
2023-12-27 11:32:22 +0300

    Update the home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function HomePage() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <h1 className="text-6xl">Hello World</h1>
+    <div className="flex h-screen flex-col items-center justify-center space-y-[40px]">
+      <h1 className="text-7xl">Metronome</h1>
+      <h1 className="text-3xl">Coming soon...</h1>
     </div>
   );
 }
