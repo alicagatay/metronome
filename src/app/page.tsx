2e9b54759b28d5e3eaa789a406c9251605499885
Ali Cagatay <aliccagatay@gmail.com>
2023-12-27 11:32:44 +0300

    Refactor function name in `page.tsx`.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-export default function HomePage() {
+export default function Page() {
   return (
     <div className="flex h-screen flex-col items-center justify-center space-y-[40px]">
       <h1 className="text-7xl">Metronome</h1>
